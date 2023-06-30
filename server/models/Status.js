8b9d9fa8460604b24f21c5eb320b35e6dcd1e7f3
Riley <94302477+Rilaey@users.noreply.github.com>
2023-06-30 18:26:54 -0400

    add: User and Status models.

---
+++ server/models/Status.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const statusSchema = new Schema({
+  message: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Status = model('Status', statusSchema)
+
+module.exports = Status;
---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = model("User", userSchema)
+
+module.exports = User;
