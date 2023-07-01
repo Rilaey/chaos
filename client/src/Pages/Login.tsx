00d2fa3352f116ace2eaf945a7ba9d60d9b42884
Riley <94302477+Rilaey@users.noreply.github.com>
2023-06-30 21:27:12 -0400

    set up materialUI in the front end.

---
+++ client/src/Pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignInForm from "../Components/SignIn/index";
+
+export default function Login() {
+  return (
+    <>
+      <SignInForm />
+    </>
+  );
+}
---
+++ client/src/Pages/SignUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUpForm from "../Components/SignUp/index"
+
+export default function SignUp() {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  )
+}
